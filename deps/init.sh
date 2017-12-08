bb6d17e6e9c49a9e69baed1da16a2213f86a250b
makepaddev <info@makepad.io>
2017-12-08 23:41:07 +0100

    hsv, colorpicker drawing, fixes

--- deps/init.sh
+++ deps/init.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/ajaxorg/ace
 git clone https://github.com/makepad/jsast
-
+git clone https://github.com/deanm/css-color-parser-js
--- project/app.js
+++ project/app.js
@@ -1,3 +1,4 @@
+'red'
 // Copyright (C) 2017 Metrological
 var mod = require('./appmodule')
 document.body.innerHTML = ''
@@ -8,13 +9,14 @@ var w = c.width = 300
 var h = c.height = 300
 cvs.style.width = w + 'px'
 cvs.style.height = h + 'px'
-var test = 'orange' 
+var test = 'orange'
+
 for(var x = 0; x < w; x++){
 	for(var y = 0; y < h; y++){
 		var r = x * 1
 		var g = y * 1
 		var b = Math.abs(x + y)
-		c.fillStyle = 'blue'
+		c.fillStyle = '#183'
 		c.fillRect(x,y,1,1)
 	}
 }
